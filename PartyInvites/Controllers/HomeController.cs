8cd4b8541a962a9e86a1fbdff7e2404e455fa73a
moganesi <moganesi@yahoo.com>
2020-06-29 19:38:11 -0700

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RsvpForm( GuestResponse guestResponse)
+        {
+            if(ModelState.IsValid) {
+                // Return where you want to go 
+            }
+            else { ModelState.AddModelError("", "could not log in"); }
+            
+            return View();
+        }
+
+    }
+}
---
+++ PartyInvites/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class Product
+    {
+        public Product(bool stock=true)
+        {
+            this.InStock = stock;
+        }
+        public String Name { get; set; }
+        public String  Category { get; set; } = "aaa";
+        public decimal? Price { get; set; }
+        public Product Related { get; set; }
+        public bool InStock { get; }
+
+    }
+}
